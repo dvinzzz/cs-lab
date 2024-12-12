c7adec2202be59a9b2cdda027bbb77c522bb9de9
dvinzzz <149927227+dvinzzz@users.noreply.github.com>
2024-12-12 21:55:25 +0530

    Add files via upload

--- DESc.py
+++ DESc.py
@@ -1,12 +1,19 @@
 import socket
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 12345))
+localhost='127.0.0.1'
+port=9090
+key=b'8bytekey'
+cipher=DES.new(key,DES.MODE_ECB)
 
-message = input("Enter message to toggle case: ")
-client.send(message.encode())
+client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.connect((localhost,port))
 
-data = client.recv(1024).decode()
-print(f"Toggled message: {data}")
+msg=input("enter a message to encrypt using the DES algo")
 
-client.close()
+encrypted_message=cipher.encrypt(pad(msg.encode(),DES.block_size))
+
+client_socket.send(encrypted_message)
+
+client_socket.close()
--- DESs.py
+++ DESs.py
@@ -1,19 +1,26 @@
 import socket
 from Crypto.Cipher import DES
-from Crypto.Util.Padding import pad
 
 localhost='127.0.0.1'
 port=9090
+
 key=b'8bytekey'
-cipher=DES.new(key,DES.MODE_ECB)
+cipher=DES.new(key, DES.MODE_ECB)
 
-client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client_socket.connect((localhost,port))
+server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket.bind((localhost,port))
+server_socket.listen(1)
 
-msg=input("enter a message to encrypt using the DES algo")
+client,address=server_socket.accept()
 
-encrypted_message=cipher.encrypt(pad(msg.encode(),DES.block_size))
+while True:
+    data=client.recv(1024)
+    if not data:
+        break
+    decrypted_data=cipher.decrypt(data).decode('utf-8').strip()
 
-client_socket.send(encrypted_message)
+    
+    print("the decrypted message is :",decrypted_data)
 
-client_socket.close()
+client.close()
+server_socket.close()
